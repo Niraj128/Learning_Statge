9e0e9e86294f548dd65f1bdfcdc860ebd63896d3
Niraj128 <101447293+Niraj128@users.noreply.github.com>
2022-11-19 11:29:46 +0530

    Update Python_set.py

--- Python_set.py
+++ Python_set.py
@@ -6,5 +6,20 @@ print(type(set2))
 
 list1 =[1,2,3,4,5,6,7,7,6,5,4,3,2,1,1,2,3]
 
-set3 = set(list1)
+set3 = list (set(list1))
 print(set3)
+
+set6 = set()
+set6.add(1)
+set6.add(4)
+set6.add(9)
+set6.add(1)
+
+#print(set6)
+
+# use of update method
+lst = [1,2,3,4,5,6,7,7,6,5,4,3,3,4,1]
+set6.update(lst)
+print(set6)
+for num in set6:
+    print(num)
